01f5e0b1fb4ac06f45e68b5d3bce712e91bd0f91
seneca <ugardun@gmail.com>
2017-12-21 09:06:33 +0100

    Add Welcome component

---
+++ src/components/WelcomeScreen/Welcome.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Welcome() {
+    return(
+        <div className="welcome">
+            <p>Welcome</p>
+        </div>
+    );
+}
+
+export default Welcome;
