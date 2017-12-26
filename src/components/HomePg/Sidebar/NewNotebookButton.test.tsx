f8f63a5baf50256fed96f44394ef11a18c075728
seneca <ugardun@gmail.com>
2017-12-26 22:33:07 +0100

    Show input on Add Notebook button click

--- src/components/HomePg/Sidebar/NewNotebookButton.test.tsx
+++ src/components/HomePg/Sidebar/NewNotebookButton.test.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import * as enzyme from 'enzyme';
+// import * as sinon from 'sinon';
 import NewNotebookButton from './NewNotebookButton';
 
 it('renders the button', () => {
   const button = enzyme.shallow(<NewNotebookButton />);
   expect(button.find('.add-notebook').text()).toEqual('Add Notebook');
+});
+
+it('renders the input', () => {
+  const button = enzyme.shallow(<NewNotebookButton />);
+  button.find('.add-notebook').simulate('click');
+  expect(button.find('.hidden').exists()).toEqual(true);
 });
--- src/components/HomePg/Sidebar/NewNotebookButton.tsx
+++ src/components/HomePg/Sidebar/NewNotebookButton.tsx
@@ -1,28 +1,51 @@
 import * as React from 'react';
 import ElectronMessager from '../../../utils/electron-messaging/electronMessager';
 import { ADD_NOTEBOOK } from '../../../constants/index';
+import './index.css';
 
 export interface Props {
     onIncrement?: () => void;
     onDecrement?: () => void;
+    showInput?: string;
 }
 
 export class NewNotebookButton extends React.Component<Props, Props> {
 
+    constructor(props: Props) {
+        super(props);
+        this.state = {showInput: 'hidden'};
+    }
+
     addNotebook(name: string) {
         ElectronMessager.sendMessageWithIpcRenderer(ADD_NOTEBOOK, name);
+    }
+
+    showInput() {
+        let showInput = this.state.showInput === 'visible' ? 'hidden' : 'visible';
+        this.setState({showInput: showInput});
     }
 
     render() {
         return (
             <div>
                 <button 
-                    onClick={() => this.addNotebook('chemistry notes')}
+                    // onClick={() => this.addNotebook('chemistry notes')}
+                    onClick={() => this.showInput()}
                     type="button"
                     className="btn btn-secondary btn-sm add-notebook"
                 >
                     Add Notebook
                 </button>
+
+                <div className={`input-group input-group-sm ${this.state.showInput}`}>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Username" 
+                        aria-label="Username" 
+                        aria-describedby="sizing-addon2"
+                    />
+                </div>
             </div>
         );
     }
