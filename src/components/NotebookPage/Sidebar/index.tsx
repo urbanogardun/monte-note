63329313047b2ab7b5a45d1c30c1106abf892ae6
seneca <ugardun@gmail.com>
2018-01-03 18:24:17 +0100

    Add note button

--- src/components/NotebookPage/Sidebar/index.tsx
+++ src/components/NotebookPage/Sidebar/index.tsx
@@ -4,10 +4,92 @@ export interface Props {
     location?: any;
 }
 
-export class Sidebar extends React.Component<Props, {}> {
+export interface State {
+    showInput: string;
+    inputValue: string;
+}
+
+export class Sidebar extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            showInput: 'hidden',
+            inputValue: '',
+        };
+    }
+
+    showInput() {
+        let showInput = this.state.showInput === 'visible' ? 'hidden' : 'visible';
+        this.setState({showInput: showInput});
+    }
+
+    // Creates notebook on Enter key press
+    handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            let notebook = this.prepareNotebook(this.state.inputValue as string);
+            this.addNotebook(notebook);
+            this.resetComponentState();
+        }
+    }
+
+    // Creates notebook when input field loses focus
+    handleFocusOut() {
+        let notebook = this.prepareNotebook(this.state.inputValue as string);
+        this.addNotebook(notebook);
+        this.resetComponentState();
+    }
+
+    // After notebook name gets submitted through the input field, resets the
+    // component state to default
+    resetComponentState() {
+        this.setState({
+            showInput: 'hidden',
+            inputValue: '',
+        });
+    }
+
+    updateInputValue(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({inputValue: e.target.value});
+    }
+
+    prepareNotebook(name: string) {
+        return name.trim();
+    }
+
+    addNotebook(name: string) {
+        if (name) {
+            console.log(name);
+            // ElectronMessager.sendMessageWithIpcRenderer(ADD_NOTEBOOK, name);
+        }
+    }
+    
     render() {
         return (
             <div className="col-sm-2 sidebar">
+                <button 
+                    onClick={() => this.showInput()}
+                    type="button"
+                    className="btn btn-secondary btn-sm add-notebook"
+                >
+                    Add Notebook
+                </button>
+
+                <div className={`input-group input-group-sm ${this.state.showInput}`}>
+                    <input 
+                        value={this.state.inputValue}
+                        onChange={e => this.updateInputValue(e)}
+                        pattern="^[a-zA-Z0-9]+$"
+                        ref={input => input && input.focus()}
+                        onKeyPress={(e) => this.handleKeyPress(e)}
+                        onBlur={() => this.handleFocusOut()}
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Note" 
+                        aria-label="Note" 
+                        aria-describedby="sizing-addon2"
+                    />
+                </div>
+
                 <h3>Notes List</h3>
             </div> 
         );
