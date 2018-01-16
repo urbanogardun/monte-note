77b570b9cca95323ff9b6e7947ee8e1184fdca56
seneca <ugardun@gmail.com>
2018-01-16 21:29:00 +0100

    Make Sidebar tests pass

--- src/components/NotebookPage/Sidebar/Sidebar.test.tsx
+++ src/components/NotebookPage/Sidebar/Sidebar.test.tsx
@@ -20,7 +20,7 @@ it('renders the sidebar', () => {
 });
 
 it('renders the input on button click', () => {
-    sidebar.find('.add-note').simulate('submit');
+    sidebar.find('.add-note-sidebar').first().simulate('submit');
     expect(sidebar.find('.hidden').exists()).toEqual(true);
 });
 
--- src/components/NotebookPage/Sidebar/index.tsx
+++ src/components/NotebookPage/Sidebar/index.tsx
@@ -155,7 +155,7 @@ export class Sidebar extends React.Component<Props, State> {
                 <section className="links">
                     <ul className="list-group notes">
                         <li 
-                            className="list-group-item sidebar-note add-note"
+                            className="list-group-item sidebar-note add-note add-note-sidebar"
                             onClick={() => this.showInput()}
                         >
                             Add Note 
