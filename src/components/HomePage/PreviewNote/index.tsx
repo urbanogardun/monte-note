d2692d87950905371c60327f4b6db9e39339e11d
seneca <ugardun@gmail.com>
2018-01-19 23:00:37 +0100

    Refactor hiding of preview note content when no note is selected

--- src/components/HomePage/PreviewNote/index.tsx
+++ src/components/HomePage/PreviewNote/index.tsx
@@ -3,6 +3,7 @@ import PreviewViewer from './PreviewViewer/index';
 import TagAdder from './TagAdder/index';
 import GoToNote from './GoToNote/index';
 import TagList from './TagList/index';
+import * as $ from 'jquery';
 
 export interface Props {
     previewContent: any;
@@ -14,43 +15,35 @@ export interface State {}
 
 export class PreviewNote extends React.Component<Props, State> {
 
-    render() {
-
-        let componentsToRender = (
-            <div className="col preview-note-content" />
-        );
-
-        // Display components only if a note has been selected for a preview
-        if (this.props.previewContent.note) {
-
-            componentsToRender = (
-                <div className="col preview-note-content">
-                    <TagAdder 
-                        tags={this.props.previewContent.tags}
-                        notebookName={this.props.previewContent.notebook}
-                        noteName={this.props.previewContent.note}
-                        updateTags={this.props.updateTags} 
-                    />
-                    <GoToNote
-                        notebookName={this.props.previewContent.notebook}
-                        noteName={this.props.previewContent.note}
-                        goToRoute={this.props.goToRoute}
-                    />
-                    <TagList 
-                        tags={this.props.previewContent.tags}
-                        notebookName={this.props.previewContent.notebook}
-                        noteName={this.props.previewContent.note}
-                        updateTags={this.props.updateTags} 
-                    />
-                    <PreviewViewer previewContent={this.props.previewContent} />
-                </div>
-            );
-
+    componentWillUpdate(nextProps: Props) {
+        if (nextProps.previewContent.note) {
+            $('.preview-note-content').children().show();
+        } else {
+            $('.preview-note-content').children().hide();
         }
+    }
 
+    render() {
         return (
             <div className="col preview-note-content">
-                {componentsToRender}
+                <TagAdder
+                    tags={this.props.previewContent.tags}
+                    notebookName={this.props.previewContent.notebook}
+                    noteName={this.props.previewContent.note}
+                    updateTags={this.props.updateTags}
+                />
+                <GoToNote
+                    notebookName={this.props.previewContent.notebook}
+                    noteName={this.props.previewContent.note}
+                    goToRoute={this.props.goToRoute}
+                />
+                <TagList
+                    tags={this.props.previewContent.tags}
+                    notebookName={this.props.previewContent.notebook}
+                    noteName={this.props.previewContent.note}
+                    updateTags={this.props.updateTags}
+                />
+                <PreviewViewer previewContent={this.props.previewContent} />
             </div>
         );
     }
