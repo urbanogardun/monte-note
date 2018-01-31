a0b6f5b4e9b1df3ea5dd69398630a0322f47699f
seneca <ugardun@gmail.com>
2018-01-31 16:30:25 +0100

    Style popover for preview viewer

--- src/utils/quill-modules/rename-attachment/renameAttachment.ts
+++ src/utils/quill-modules/rename-attachment/renameAttachment.ts
@@ -42,7 +42,7 @@ function renameAttachment(quill: Quill, options?: any) {
         <div class="attachment-text">
             <span title="Open File" class="oi oi-file attachment-link"></span>
             <span class="separator">|</span> 
-            <span title="Open in Explorer" class="oi oi-external-link attachment-open-external"></span>
+            <span title="Open in Explorer" class="oi oi-external-link attachment-open-external-normal"></span>
         </div>
         <div class="attachment-input input-group input-group-sm mb-3">
             <div class="input-group-prepend">
