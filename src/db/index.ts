c091343486c06d69c5f4a12a72eae9c49a8ee574
seneca <ugardun@gmail.com>
2018-02-18 16:11:58 +0100

    Save app data to montenote-data.json

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,27 +2,13 @@ const electron = require('electron');
 const path = require('path');
 const Datastore = require('nedb');
 
-// const userDataPath = (electron.app || electron.remote.app).getPath('userData');
-// const dbName = 'app-data';
-// const db = new Datastore({ filename: path.join(userDataPath, dbName + '.json'), autoload: true });
-
-// // Bootstrap db with notebooks entry
-// db.find({ notebooks: [] }, (err: any, docs: any) => {
-//     console.log(docs.length);
-//     if (docs.length === 0) {
-//         db.insert({ notebooks: [], name: 'notebooks' });
-//     }
-// });
-
-// export default db;
-
 export class Db {
     
     db: Nedb;
     
     constructor() {
         let userDataPath = (electron.app || electron.remote.app).getPath('userData');
-        let dbName = 'app-data';
+        let dbName = 'montenote-data';
         this.db = new Datastore(
             { 
                 filename: path.join(userDataPath, dbName + '.json'), 
