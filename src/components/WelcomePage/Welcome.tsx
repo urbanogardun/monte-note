d4744877ac82072701fd7516f32d52155ee80da0
seneca <ugardun@gmail.com>
2018-02-18 13:00:47 +0100

    Style welcome page

--- src/components/WelcomePage/Welcome.tsx
+++ src/components/WelcomePage/Welcome.tsx
@@ -18,15 +18,37 @@ class Welcome extends React.Component<Props, object> {
 
     render() {
         return (
-            <div className="welcome">
-                <h1>Welcome</h1>
-                <button
-                    className="btn btn-primary btn-lg"
-                    onClick={() => { this.chooseNotebooksLocation(); }}
-                >
-                    Click Me
-                </button>
+            <div className="container-fluid welcome-screen">
+                <div className="row align-items-center welcome-row">
+                    <div className="col montenote-welcome">
+                        <h1 className="display-4">Welcome!</h1>
+                        <p className="lead">
+                            To begin using MonteNote, please select a directory
+                            in which you want your note content to be saved. If you
+                            would like to import notebooks you already have, just
+                            select the directory that has them and MonteNote will import
+                            them for you.
+                    </p>
+                        <button 
+                            type="button" 
+                            className="btn btn-primary select-notebooks-directory"
+                            onClick={() => { this.chooseNotebooksLocation(); }}
+                        >
+                        Primary
+                        </button>
+                    </div>
+                </div>
             </div>
+
+            // <div className="welcome">
+            //     <h1>Welcome</h1>
+            //     <button
+            //         className="btn btn-primary btn-lg"
+            //         onClick={() => { this.chooseNotebooksLocation(); }}
+            //     >
+            //         Click Me
+            //     </button>
+            // </div>
         );
     }
 }
